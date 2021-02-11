b85c3a4aee5b223b991cf6aebbfe5968011e0e51
Aman1777Mishra <77769940+Aman1777Mishra@users.noreply.github.com>
2021-02-11 19:51:22 +0530

    Add files via upload

---
+++ Length.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+	int x=225;
+	char y='Y';
+	float z=29.99;
+	double a=943.2291;
+	cout<<"Size of int :"<<sizeof(x)<<"bytes"<<endl;
+	cout<<"Size of char:"<<sizeof(y)<<"byte"<<endl;
+	cout<<"Size of float :"<<sizeof(z)<<"bytes"<<endl;
+	cout<<"Size of double :"<<sizeof(a)<<"bytes"<<endl;  
+	return 0;
+}
