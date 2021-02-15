d30eaf6de098a57255b5259f64fb817fc05659c7
Aman1777Mishra <77769940+Aman1777Mishra@users.noreply.github.com>
2021-02-15 19:45:09 +0530

    Add files via upload

---
+++ Multiply.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+    cout<<"Enter first number :";
+    cin>>x;
+    cout<<"Enter second number :";
+    cin>>y;
+    cout<<"Multiplication of the given numbers is :"<<endl<< "\t"<<x<<"*"<<y<<"="<<x*y;
+    return 0;
+}
